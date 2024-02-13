13d6ac332c9dc3ad40a94be6d227934effca5753
Aman Kumar <amankumarkr111@gmail.com>
2024-02-13 12:46:06 +0530

    Navbar Completed

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,6 +1,6 @@
-const ContactPage = () => {
-    return <div className="">ContactPage</div>;
+const PortfolioPage = () => {
+    return <div className="">PortfolioPage</div>;
   };
   
-  export default ContactPage;
+  export default PortfolioPage;
   
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,85 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import NavLink from "./navLink";
+
+const links = [
+  { url: "/", title: "Home" },
+  { url: "/about", title: "About" },
+  { url: "/portfolio", title: "Portfolio" },
+  { url: "/contact", title: "Contact" },
+];
+
 const Navbar = () => {
-  return <div className="">Navbar</div>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+      {/* Links */}
+      <div className="hidden md:flex gap-4 w-1/3">
+        {links.map((link) => (
+          <NavLink link={link} key={link.title}/>
+        ))}
+      </div>
+      {/*Logo*/}
+      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
+        <Link
+          href="/"
+          className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
+        >
+          <span className="text-white mr-1">hekshot</span>
+          <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
+            .dev
+          </span>
+        </Link>
+      </div>
+      {/* Social */}
+      <div className="hidden md:flex gap-4 w-1/3">
+        <Link href="/">
+          <Image src="/github.png" alt="github" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/dribbble.png" alt="dribbble" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/facebook.png" alt="facebook" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/pinterest.png" alt="pinterest" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/instagram.png" alt="instagram" width={24} height={24} />
+        </Link>
+        <Link href="/">
+          <Image src="/linkedin.png" alt="linkedin" width={24} height={24} />
+        </Link>
+      </div>
+      {/*Responsive Menu*/}
+      <div className="md:hidden">
+        {/*Menu button*/}
+        <button
+          className="w-10 h-8 flex flex-col justify-between z-50 relative"
+          onClick={() => setOpen(!open)}
+        >
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
+        </button>
+        {/* Menu List */}
+        {open && (
+          <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+            {links.map((link) => (
+              <Link href={link.url} key={link.title}>
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
